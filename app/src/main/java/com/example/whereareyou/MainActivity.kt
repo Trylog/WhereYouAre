ae286769e8ce505a8b62de7dc0b6c91b06a31b8c
Michał Bernacki-Janson <94766391+Trylog@users.noreply.github.com>
2024-11-23 21:18:56 +0100

    live location updating, displays distance and lines between markers

--- app/src/main/java/com/example/whereareyou/MainActivity.kt
+++ app/src/main/java/com/example/whereareyou/MainActivity.kt
@@ -1,42 +1,30 @@
-@file:OptIn(ExperimentalPermissionsApi::class)
 
 package com.example.whereareyou
 
 import android.annotation.SuppressLint
 import android.app.Activity
-import android.content.Context
 import android.location.Location
 import android.os.Bundle
-import android.os.strictmode.UntaggedSocketViolation
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.whereareyou.ui.theme.WhereAreYouTheme
-import com.google.android.gms.maps.model.CameraPosition
-import com.google.maps.android.compose.GoogleMap
-import com.google.maps.android.compose.rememberCameraPositionState
-import com.google.android.gms.maps.model.LatLng
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
-import com.google.accompanist.permissions.*
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
-import com.google.maps.android.compose.MapProperties
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.Polyline
+import com.google.maps.android.compose.rememberCameraPositionState
 import com.google.maps.android.compose.rememberMarkerState
 import kotlinx.coroutines.tasks.await
 
@@ -72,8 +60,8 @@ fun CurrentLocation(onLocationChanged: (Location?) -> Unit) {
     if (hasPermission) {
         LaunchedEffect(Unit) {
             location = getLastKnownLocation(fusedLocationClient)
+            onLocationChanged(location)
         }
-        if(location != null)onLocationChanged(location)
     } else {
         val activity = context as? Activity
         activity?.requestPermissions(
@@ -94,33 +82,60 @@ suspend fun getLastKnownLocation(client: FusedLocationProviderClient): Location?
     }
 }
 
+fun calculateDistance(start: Location, end: LatLng):Float{
+    val newLoc = Location("new")
+    newLoc.latitude = end.latitude
+    newLoc.longitude = end.longitude
+    return start.distanceTo(newLoc) / 1000
+}
 
 @Composable
 fun MapScreen(users: ArrayList<Pair<String, LatLng>>) {
     //get location
     var location by remember { mutableStateOf<Location?>(null) }
 
-    CurrentLocation { n -> location = n }
-
-    val currentPosition = if(location != null)LatLng(location!!.latitude, location!!.longitude) else LatLng(-33.852, 151.211) // Lokalizacja w Sydney
     val cameraPositionState = rememberCameraPositionState {
-        position = CameraPosition.fromLatLngZoom(currentPosition, 10f)
+        position = CameraPosition.fromLatLngZoom(LatLng(-33.852, 151.211), 10f)
+    }
+
+    CurrentLocation { n ->
+        location = n
+        n?.let {
+            cameraPositionState.position = CameraPosition.fromLatLngZoom(
+                LatLng(it.latitude, it.longitude),
+                10f
+            )
+        }
     }
 
     GoogleMap(
         modifier = Modifier.fillMaxSize(),
         cameraPositionState = cameraPositionState
+
     ){
-        Marker(
-            state = rememberMarkerState(position = currentPosition),
-            title = "Moja lokalizacja",
-        )
+        val currentPosition = location?.let { LatLng(it.latitude, it.longitude) }
+        if (currentPosition != null) {
+            Marker(
+                state = rememberMarkerState(position = currentPosition),
+                title = "Moja lokalizacja",
+            )
+        }
+
         for (user in users) {
             Marker(
                 state = rememberMarkerState(position = user.second),
-                title = user.first
+                title = user.first,
+                snippet = location?.let {
+                    "Odległość: " + String.format("%.2f",calculateDistance(it, user.second)) + "km"
+                } ?: "Lokalizacja niedostępna"
             )
+            if (currentPosition != null) {
+                Polyline(
+                    points = listOf(currentPosition, user.second),
+                    color = androidx.compose.ui.graphics.Color.Blue,
+                    width = 5f
+                )
+            }
         }
     }
-
 }
